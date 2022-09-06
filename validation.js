0c93c7d76019945a46d72219bfbaae22914abf71
Geet <geet.mundhe@gmail.com>
2022-09-06 13:50:22 +0530

    update

--- validation.js
+++ validation.js
@@ -1,69 +1,53 @@
-function whitespacevalidation(){
-   var firstname1 = document.getElementById('firstname').value;
-if(firstname1.replace( /\s/, "").length <= 0){
-   alert("no whitespace is allowed");
-}
-
-
-/*var firstname1 =document.getElementById('firstname').value;     
-if (firstname1 == "")
-{
-alert("enter valid first name!");
-return false;
-}
-
-var lastname1 =document.getElementById('lastname').value;
-if (lastname1 == "")
-{
-alert("enter valid Last name!");
-return false;
-}*/
-/*function whitespacevalidation() {
-	var firstname = document.getElementById('firstname').value;
+function submitSurvey() { 
+	var firstname = document.getElementById('first-name').value;
 	var checkFirstName = /\s/g.test(firstname);
+	
+	var lastname = document.getElementById('last-name').value;
+	var checkLastName = /\s/g.test(lastname);
+	
+	var username = document.getElementById('username').value;	
+	var regExUsername = /^[a-z0-9]+$/i;
+	
+	var email = document.getElementById('email').value;
+	var checkEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+	
+	var contact = document.getElementById('contact').value;
+	
+	var postal_code = document.getElementById('postal-code').value;
+	
 	if(firstname == '' || checkFirstName) {
 		alert("Enter valid first name!");
 		return false;			
 	}
+	else if(lastname == '' || checkLastName) {
+		alert("Enter valid last name!");
+		return false;			
+	}
+	else if(lastname == '') {
+		alert("Enter valid username!");
+		return false;			
+	}
 	
-}*/
-//////username validation/////
-var username= document.getElementById('username').value;
-var regEx = /^[A-Za-z0-9\s]+$/;
-if(username == regEx){
-//alert("wd");
-   //}else{
-    alert("enter valid username");
-   }
-   
-   //////email validation//// 
-   var  email = document.getElementById('email').value;
-   var emailregex = /^([a-z 0-9\.-]+)+@([a-z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
-    if(emailregex.test(email)){
-      alert("valid");
-}else{
-   alert("Enter valid email!");
+    else if(!username.match(regExUsername))  {
+		alert("Enter valid username!");
+      	return false;
+    }
+	
+	else if(email == '' || !checkEmail) {
+		alert("Enter valid email!");
+		return false;			
+	}
+	
+	else if(contact == '' || isNaN(contact) || contact.length!=10) {
+		alert("Enter valid contact!");
+		return false;			
+	}
+	
+	else if(postal_code == '' || isNaN(postal_code) || postal_code.length!=6) {
+		alert("Enter valid postal code!");
+		return false;			
+	}
+	else { 
+		alert("Submitted");
+	}	
 }
-
-///phone number validation////
-   var phoneno = document.getElementById('contact').value;
-   //var con =/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-   if(phoneno.length<10 ||phoneno.length>10 ){
-     alert("Enter valid contact!");
-
-  // }else{
-      alert("Enter valid contact");
-   //}
-}
-
-}
-//var firstname1 =document.getElementById('firstname').value;
-
-//var firstname =text.match(/\s/g);
-    
-        // Check for white space
-      //  if (firstname1) {
-           // alert("Please Check Your Fields For Spaces");
-           // return 'true';
-       // }
- 
